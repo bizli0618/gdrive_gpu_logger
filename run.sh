4b9f57326f25f343e8c58a26805e3c8c5ad0624f
TaeHan Lee <45648726+andylee-24@users.noreply.github.com>
2025-08-16 14:17:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 export ROW=2
 export SERVER_NAME="SERVER_NAME"
-export SHEET_ID="GPU"
+export SHEET_ID="your_sheet_id"
 export GOOGLE_APPLICATION_CREDENTIALS="your_path.json"
 
 watch -n 60 'python update_gsheet.py'
