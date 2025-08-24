1baa66deb4cfee1b73db80553d894fe42ddd8fa8
TaeHan Lee <45648726+andylee-24@users.noreply.github.com>
2025-08-24 14:13:30 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ export SERVER_NAME="SERVER_NAME"
 export SHEET_ID="<sheet_id>"                                 # https://docs.google.com/spreadsheets/d/<sheet_id>/edit ...
 export GOOGLE_APPLICATION_CREDENTIALS="your_key_path.json"
 
+# update sheet every 60 seconds
 watch -n 60 'python update_gsheet.py'
