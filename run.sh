8df7d12bc04accb9d529337dcced436d4a687895
JeongBin Lee <63985988+bizli0618@users.noreply.github.com>
2025-08-25 13:33:21 +0900

    Add user/process GPU logging modes

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@ set -euo pipefail
 
 export ROW=2
 export SERVER_NAME="SERVER_NAME"
-export SHEET_ID="<sheet_id>"                                 # https://docs.google.com/spreadsheets/d/<sheet_id>/edit ...
+export SERVER_TYPE="internal"              # or "external"
+export SHEET_ID="<sheet_id>"                # https://docs.google.com/spreadsheets/d/<sheet_id>/edit ...
+export SHEET_TAB_USERS="GPU_USERS"          # worksheet for internal servers
+export SHEET_TAB_PROCS="GPU_PROCS"          # worksheet for external servers
 export GOOGLE_APPLICATION_CREDENTIALS="your_key_path.json"
 
 # update sheet every 60 seconds
